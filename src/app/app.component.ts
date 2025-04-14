b5c928cfe09e2ddb933e2baa3671c027cc1ecc3c
Fernando Luis <fernando@Fernandos-MacBook-Pro.local>
2025-04-14 15:02:52 -0300

    add forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
 import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'firebase';
+  email = '';
+  senha = '';
+  constructor(private autenticador: Auth) {}
 
-  constructor(autenticador: Auth) {
-    signInWithEmailAndPassword(autenticador, 'ferasdasd', '123');
+  logar() {
+    signInWithEmailAndPassword(
+      this.autenticador,
+      'usuario@iffarroupilha.edu.br',
+      'senha'
+    );
   }
 }
